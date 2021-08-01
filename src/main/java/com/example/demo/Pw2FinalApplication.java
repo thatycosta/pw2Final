cbc763fac7163cbdb477928cfb70012bb30b6610
Tauane Costa <tauanedasilvacosta@gmail.com>
2021-08-01 18:09:17 -0300

    First Commit

---
+++ src/main/java/com/example/demo/Pw2FinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pw2FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pw2FinalApplication.class, args);
+	}
+
+}
